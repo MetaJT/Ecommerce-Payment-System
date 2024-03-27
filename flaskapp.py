d9b88f27a5c388cd7b087dd1f7d0364dee4fb287
MetaJT <126602778+MetaJT@users.noreply.github.com>
2024-03-26 21:44:29 -0500

    Add files via upload
    Initial Flask commit

---
+++ flaskapp.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import pymysql
+
+app = Flask(__name__)
+
+# Initialize MySQL
+db = pymysql.connect(
+    host='dbecommerce.c3okqegi6lyw.us-east-2.rds.amazonaws.com',
+    user='admin',
+    password='DBMS2024!',
+    database='ecommerceDB'
+)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
