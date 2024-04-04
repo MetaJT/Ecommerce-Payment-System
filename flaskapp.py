2a757ffce8af6df3f471f94ff9704c0632cb4a3f
metaJT <jordantrotter3@gmail.com>
2024-04-03 21:47:54 -0500

    Added user-logging

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,30 @@
 from flask import Flask, render_template, request, flash, url_for, redirect, request, jsonify
+from flask_login import login_user, logout_user, UserMixin, LoginManager, current_user
 import pymysql
 from schema import create_queries
 
 app = Flask(__name__)
 app.secret_key = 'key'
+login_manager = LoginManager(app)
+
+# Stores the current user info
+class User(UserMixin):
+    def __init__(self, user_info):
+        self.id = str(user_info['UserID'])
+        self.info = dict(user_info)
+
+    # Gets all the info attached to account from the DB
+    # Stored as a dictionary
+    def info(): 
+        connection = get_db_connection()
+        try:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM `ecommerceDB`.`Users` WHERE UserID=%s;"
+                cursor.execute(sql, (id,))
+                result = cursor.fetchall()
+                return result
+        finally:
+            connection.close()
 
 # Initialize MySQL
 def get_db_connection():
@@ -26,38 +47,43 @@ def create_tables(sql_file):
     finally:
         connection.close()
 
+create_tables('schema.py')
+
 @app.route('/')
 @app.route('/home')
 @app.route('/index')
 
 def index():
-    create_tables('schema.py')
+    connection = get_db_connection()
     try:
-        connection = get_db_connection()
         with connection.cursor() as cursor:
             sql = "SELECT * FROM `ecommerceDB`.`Users`;" # using Users table instead of users
             cursor.execute(sql)
             result = cursor.fetchall()
-            for i in result:
-                print(i)
     finally:
         connection.close()
-    return render_template('Users.html', users=result)
+    # If a user is logged in -> pass currUser info to HTML
+    if current_user.is_authenticated: 
+        return render_template('users.html', users=result, currUser=current_user.info)
+    else:
+        return render_template('users.html', users=result)
 
-def get_user_names():
+def get_users():
     connection = get_db_connection()
-    cursor = connection.cursor()
-    cursor.execute('SELECT firstName FROM `ecommerceDB`.`Users`;')
-    user_names = cursor.fetchall()
-    cursor.close()
-    connection.close()
-    return user_names
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `ecommerceDB`.`Users`;"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+    finally:
+        connection.close()
+    return result
 
 # This route is used for the js request, not ideal to keep
 # Will try and do something with this
-@app.route('/login-user', methods=['GET',])
-def login_user():
-    userID = request.args.get('userID', default='1')
+@app.route('/user-login', methods=['GET',])
+def user_login():
+    userID = request.args.get('userID')
     try:
         connection = get_db_connection()
         with connection.cursor() as cursor:
@@ -71,11 +97,51 @@ def login_user():
     finally:
         connection.close()
 
-@app.route('/login')
+# Responsible for loading the user object for Flask-Login
+@login_manager.user_loader
+def load_user(user_id):
+    connection = get_db_connection()
+    try:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `ecommerceDB`.`Users` WHERE UserID=%s;"
+            cursor.execute(sql, (user_id,))
+            user_data = cursor.fetchone()
+            if user_data:
+                # Passes UserID from the DB and then all
+                return User(user_data)
+            else:
+                return None  # User not found
+    finally:
+        connection.close()
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    user_names = get_user_names()
-    return render_template('login.html', user_names=user_names)
-    
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        connection = get_db_connection()
+        try:
+            with connection.cursor() as cursor:
+                sql = "SELECT * FROM `ecommerceDB`.`Users` WHERE Email=%s;" # using Users table instead of users
+                cursor.execute(sql, (email,))
+                connection.commit()
+                result = cursor.fetchone()
+                # creating user object to pass to Flask-Login::login_user
+                user = User(result) 
+                if result:
+                    flash('Logged in successfully!', category='success')
+                    login_user(user) # Logs user in
+                    return redirect(url_for('index'))
+                else:
+                    flash('No account found with that email!')
+        finally:
+            connection.close()
+    return render_template('login.html', users=get_users())
+
+@app.route('/logout')
+def logout():
+    logout_user() # Logs user out
+    return redirect(url_for('index'))
 
 @app.route('/about')
 def about():
@@ -92,20 +158,20 @@ def create_account():
         last_name = request.form['last_name']
         username = request.form['username']
         email = request.form['email']
-        # password = request.form['password']
+        password = request.form['password']
 
         try:
             connection = get_db_connection()
             with connection.cursor() as cursor:
-                sql = "INSERT INTO ecommerceDB.Users (FirstName, LastName, Username, Email) VALUES (%s, %s, %s, %s)" # Need to add password
-                cursor.execute(sql, (first_name, last_name, username, email))
+                sql = "INSERT INTO ecommerceDB.Users (FirstName, LastName, Username, Email, Password) VALUES %s" # Need to add password
+                cursor.execute(sql, (first_name, last_name, username, email, password))
                 connection.commit()
                 print("Successfully created user.")
         except Exception as e:
             print(f"Error creating user: {str(e)}")
         finally:
             connection.close()
-        return redirect(url_for('index')) # Send user back to home page
+        return redirect(url_for('index'))
     return render_template('create_account.html')
 
 if __name__ == '__main__':
