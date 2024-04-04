26f5cdbb6658ac4143fef8fa7467617795ab7700
metaJT <jordantrotter3@gmail.com>
2024-04-03 21:44:08 -0500

    Modified to address for any amount of buttons

--- static/loginscript.js
+++ static/loginscript.js
@@ -2,14 +2,15 @@
 function signInUser(userId) {
     // Make an AJAX request to Flask backend
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/login-user?userID=' + userId, true);
+    xhr.open('GET', '/user-login?userID=' + userId, true);
     xhr.onload = function() {
         if (xhr.status == 200) {
             console.log(xhr.responseText);
             var response = JSON.parse(xhr.responseText);
             if (response) {
                 console.log(response.user.FirstName);
-                document.getElementById('user').innerText = response.user.FirstName;
+                document.getElementById('email').value = response.user.Email;
+                document.getElementById('password').value = response.user.Password;
             } else {
                 // Handle error or no username found
                 console.error('No username found');
@@ -19,21 +20,10 @@ function signInUser(userId) {
     xhr.send();
 }
 document.addEventListener('DOMContentLoaded', function() {
-    // Your JavaScript code here
-    var alexButton = document.getElementById('Alex');
-    var jordanButton = document.getElementById('Jordan');
-    var barackButton = document.getElementById('Barack');
-
-    // Add event listeners to buttons
-    jordanButton.addEventListener('click', function() {
-        signInUser('1');
-    });
-
-    alexButton.addEventListener('click', function() {
-        signInUser('2');
-    });
-
-    barackButton.addEventListener('click', function() {
-        signInUser('3');
-    });
+    var userButtons = document.querySelectorAll('.user-button');
+    userButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            signInUser(button.id);
+        })
+    })
 });
